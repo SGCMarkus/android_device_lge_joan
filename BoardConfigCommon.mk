d0b6f41a00e3c449efb5887a25170988b4035503
SGCMarkus <markusornik@gmail.com>
2018-06-06 15:40:40 +0200

    joan-common: set proper product model on boot
    Change-Id: I3237ae1766946d66396fa08588093f39e981807a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,6 +125,10 @@ DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_lge_msm8998
+TARGET_RECOVERY_DEVICE_MODULES := libinit_lge_msm8998
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=joan user_debug=31 msm_rtb.filter=0x37
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 lpm_levels.sleep_disabled=1
