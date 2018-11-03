1a3beab5e904cd561c3bd653f76dd7332ecc24de
SGCMarkus <markusornik@gmail.com>
2018-11-03 14:49:31 +0100

    joan-common: introduce our own vibration service
    * LGs immvibed caused issues after a certain amount of vibration and needed to be restarted (with default vibration service)
    * or phone had to be rebooted
    
    (cherry picked from commit d402f7ebcb836939504ab65c5eda5db7e4276d72)

--- joan.mk
+++ joan.mk
@@ -368,7 +368,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.2-service.joan
 
 # VR HAL
 PRODUCT_PACKAGES += \
