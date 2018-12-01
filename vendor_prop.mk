57f4959af0f878e357822281d7ee89031d90e1bd
SGCMarkus <markusornik@gmail.com>
2018-12-01 19:32:02 +0100

    joan-common: update vendor props to november security patch
    Change-Id: Ib55063c7ea2bbca1a21aeeaae94594e1c67e728b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -110,6 +110,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.use_data_netmgrd=true \
     telephony.lteOnCdmaDevice=1
 
+# Security Patch Level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2018-11-01
+
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qti.sensors.dev_ori=true \
