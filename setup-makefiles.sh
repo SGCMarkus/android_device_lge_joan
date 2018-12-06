866ef5340a86c31a001401a81fca61b9b1c14f40
SGCMarkus <markusornik@gmail.com>
2018-12-06 18:45:48 +0100

    joan-common: rename us998 to h932... there no us998 tree, but a h932 one
    Change-Id: I970b580fd15649fdea314d127f2cb4695bb364e7

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -37,7 +37,7 @@ fi
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and common guards
-write_headers "h930 us998"
+write_headers "h930 h932"
 
 write_makefiles "$MY_DIR"/proprietary-files.txt true
 
