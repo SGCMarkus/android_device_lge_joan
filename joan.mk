d2997d3dd6f75065e248ea7e3c666390a30bca97
SGCMarkus <markusornik@gmail.com>
2019-01-06 21:05:20 +0100

    joan-common: add Pocketmode
    * based on device/oneplus/sdm845 rewritten to work on the V30
    
    Change-Id: I25a21adb7cd0c4351f153835b3022c903b1a687a

--- joan.mk
+++ joan.mk
@@ -149,7 +149,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    LGEPocketMode
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
