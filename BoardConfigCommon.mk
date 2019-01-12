58b973a04470b86bdb352523bbffd76a2b8e43d5
SGCMarkus <markusornik@gmail.com>
2019-01-12 10:34:17 +0100

    joan-common: go enforcing
    Change-Id: Id690189f05eefde2eb2c2286729a62fc4cdfaaa9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,7 +138,7 @@ BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000
