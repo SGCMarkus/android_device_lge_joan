a0bea9e4e4627cb5ceabe581a2c41124ff097fad
SGCMarkus <markusornik@gmail.com>
2019-04-08 09:07:32 +0200

    joan-common: remove unneeded immvibed prop
    Change-Id: Id5c3c936a8a246fac993b2ed4bd9b9b15c7c4711

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -133,10 +133,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sys.usb.ffs.aio_compat=1 \
     persist.sys.usb.config.extra=none
 
-# Vibration
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.immvibed.fakeandrvers=8
-
 # Voice assistant
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opa.eligible_device=true
