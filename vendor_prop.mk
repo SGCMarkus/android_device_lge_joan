8cbe4edbe0ec6511df934598e0878f5efd60cf88
SGCMarkus <markusornik@gmail.com>
2019-09-13 17:34:37 +0200

    joan-common: set surface_flinger props for HDR display
    Change-Id: I328fdf308a4fd098e7a2f293634b298befa8501e

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -222,6 +222,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696 \
     debug.sf.early_phase_offset_ns=1500000 \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
