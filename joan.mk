b26422f9ead99ac29f68f772738d3fd94cecce4e
SGCMarkus <markusornik@gmail.com>
2020-04-21 14:41:07 +0200

    joan-common: readd lineage overlays, but exclude them from rro
    Change-Id: I6a6c09d7c555ec23803efe33fef22a988411e927

--- joan.mk
+++ joan.mk
@@ -24,9 +24,10 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay \
-#    $(COMMON_PATH)/overlay-lineage
+    $(COMMON_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(COMMON_PATH)/overlay-lineage/lineage-sdk
 
 # Properties
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
