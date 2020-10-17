083fb4b88a5ba4ec29e399e2a90ec45e6f1e858a
Lance <Gero3977@gmail.com>
2020-10-17 10:06:01 -0700

    joan: add correct permissions to h932 blobs
    and specify we're only dealing with graphics.

--- releasetools/device_check.sh
+++ releasetools/device_check.sh
@@ -15,16 +15,22 @@
 # limitations under the License.
 #
 
-echo "Checking if we are on a H932 (not H932PR). H932 requires specific blobs."
+echo "Checking if we are on a H932 (not H932PR). H932 requires specific graphics blobs."
 
 if cat /proc/cmdline | grep -q "LG-H932"; then
     echo "H932 detected, copying blobs..."
     mv /system/system/vendor/firmware/H932/* /system/system/vendor/firmware/
 else
-    echo "Not a H932, copy over the H930 specific blobs"
+    echo "Not a H932, copy over the H930 specific graphics blobs"
     mv /system/system/vendor/firmware/H930/* /system/system/vendor/firmware/
 fi
 
 echo "Remove unneeded blobs"
 rm -r /system/system/vendor/firmware/H930
 rm -r /system/system/vendor/firmware/H932
+
+echo "Set proper permissions for newly copied graphics blobs"
+chmod 0644 /system/system/vendor/firmware/a540*
+chown root:root /system/system/vendor/firmware/a540*
+echo "including SELinux file contexts"
+chcon u:object_r:firmware_file:s0 /system/system/vendor/firmware/a540*
