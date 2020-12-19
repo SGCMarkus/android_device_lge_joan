7ff621c2b3eada1b3edae81c02a734906161fd42
SGCMarkus <markusornik@gmail.com>
2020-12-19 12:07:32 +0100

    joan: build libhidltransport and libhwbinder

--- device.mk
+++ device.mk
@@ -217,7 +217,11 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # Init
 PRODUCT_COPY_FILES += \
