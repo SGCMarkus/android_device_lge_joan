83c038d69ea12accc4d49f9ca9145aaf55e9f07c
SGCMarkus <markusornik@gmail.com>
2020-12-19 12:29:55 +0100

    joan: set launched with n_mr1

--- lineage_joan.mk
+++ lineage_joan.mk
@@ -1,6 +1,8 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
