24be59d959a8e93564bc8aaa5b04223b4f848edc
SGCMarkus <markusornik@gmail.com>
2020-12-19 13:13:06 +0100

    joan: update SEPolicy include dir

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -204,7 +204,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 #BOARD_SEPOLICY_DIRS += device/lge/joan-common/sepolicy/vendor
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/lge/joan-common/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
