5a00a80de9cac228c0f1a015ecb9e676c6a99016
SGCMarkus <markusornik@gmail.com>
2021-02-22 09:58:58 +0100

    joan: adapt release tools to use different toy/busyboxes, and work with multiple recoveries

--- releasetools/device_check.sh
+++ releasetools/device_check.sh
@@ -17,20 +17,42 @@
 
 echo "Checking if we are on a H932 (not H932PR). H932 requires specific graphics blobs."
 
+# Check for toy/busybox
+TOYBOX=/sbin/toybox
+BUSYBOX=/sbin/busybox
+
+mkdir -p /mnt/system
+
+if test -f "$TOYBOX"; then
+    toybox mount /dev/block/bootdevice/by-name/system -t ext4 /mnt/system
+elif test -f "$BUSYBOX"; then
+    busybox mount /dev/block/bootdevice/by-name/system -t ext4 /mnt/system
+else
+    /tmp/toybox mount /dev/block/bootdevice/by-name/system -t ext4 /mnt/system
+fi
+
 if cat /proc/cmdline | grep -q "LG-H932"; then
     echo "H932 detected, copying blobs..."
-    mv /system/system/vendor/firmware/H932/* /system/system/vendor/firmware/
+    mv /mnt/system/system/vendor/firmware/H932/* /mnt/system/system/vendor/firmware/
 else
     echo "Not a H932, copy over the H930 specific graphics blobs"
-    mv /system/system/vendor/firmware/H930/* /system/system/vendor/firmware/
+    mv /mnt/system/system/vendor/firmware/H930/* /mnt/system/system/vendor/firmware/
 fi
 
 echo "Remove unneeded blobs"
-rm -r /system/system/vendor/firmware/H930
-rm -r /system/system/vendor/firmware/H932
+rm -r /mnt/system/system/vendor/firmware/H930
+rm -r /mnt/system/system/vendor/firmware/H932
 
 echo "Set proper permissions for newly copied graphics blobs"
-chmod 0644 /system/system/vendor/firmware/a540*
-chown root:root /system/system/vendor/firmware/a540*
+chmod 0644 /mnt/system/system/vendor/firmware/a540*
+chown root:root /mnt/system/system/vendor/firmware/a540*
 echo "including SELinux file contexts"
-chcon u:object_r:firmware_file:s0 /system/system/vendor/firmware/a540*
+chcon u:object_r:firmware_file:s0 /mnt/system/system/vendor/firmware/a540*
+
+if test -f "$TOYBOX"; then
+    toybox umount /mnt/system
+elif test -f "$BUSYBOX"; then
+    busybox umount /mnt/system
+else
+    /tmp/toybox umount /mnt/system
+fi
--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -15,7 +15,5 @@
 #
 
 def FullOTA_InstallEnd(info):
-    info.script.Mount("/system");
     info.script.AppendExtra('assert(run_program("/tmp/install/bin/device_check.sh") == 0);');
-    info.script.Unmount("/system");
 
