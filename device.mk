0293ce636f4b7b57aed59343ccb750a78c520af9
SGCMarkus <markusornik@gmail.com>
2021-03-07 15:14:35 +0100

    joan: switch back to prebuilt fingerprint hal
    * whatever LG is doing, the source built service only works on oreo modem
    with pie modem you cant register new fingerprints (but read them if you set them up with oreo modem)

--- device.mk
+++ device.mk
@@ -187,7 +187,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.lge_msm8998 \
+    android.hardware.biometrics.fingerprint@2.1 \
     LGEPocketMode
 
 # FM packages
